1fcb42e739532377d00fe53f47624d215b0e8fb1
Ameet Pradhan <jimmy.4244@gmail.com>
2020-01-14 06:32:23 +0530

    image with preview

---
+++ public/js/dashboard.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $(".card")
+    .addClass("shadow-lg p-3 mb-5 bg-white rounded ")
+    .attr("min-width", "80px");
+  $(".avatar").addClass("shadow-sm p-3 mb-5 bg-white rounded");
+});
